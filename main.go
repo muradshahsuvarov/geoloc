30321d9d472c05b239cb0a90219bb3124a0e9a0d
Murad Shahsuvarov <42478293+muradshahsuvarov@users.noreply.github.com>
2023-04-25 11:30:32 -0230

    Update main.go

--- main.go
+++ main.go
@@ -8,99 +8,80 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"sync"
 )
 
 type server struct{}
 
-// Define a mutex to synchronize access to the cache map
-var cacheMutex sync.Mutex
-
-// getContent fetches data from the Overpass API based on the specified parameters.
 func getContent(radius string, LAT float64, LNG float64, keyInput string, valueInput string) ([]byte, error) {
-	// Construct the Overpass API query URL using the input parameters
-	queryURL := fmt.Sprintf("https://overpass-api.de/api/interpreter?data=(node(around:%s,%v,%v)[%s='%s'][name];>;);out;", radius, LAT, LNG, keyInput, valueInput)
-
-	// Send a GET request to the Overpass API with the constructed URL
-	resp, err := http.Get(queryURL)
+	resp, err := http.Get("https://overpass-api.de/api/interpreter?data=(node(around:" + radius + "," + fmt.Sprintf("%v", LAT) + "," + fmt.Sprintf("%v", LNG) + ")[" + keyInput + "='" + valueInput + "'][name];>;);out;")
+	
 	if err != nil {
-		// Log the error if the GET request failed
-		log.Printf("Error getting content: %v", err)
-		return nil, err
+	log.Printf("error in getting the content is: %v", err)
 	}
 
-	// Ensure that the response body is closed after the function returns
+
 	defer resp.Body.Close()
 
-	// If the response status code is not OK, return an error
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("status error: %v", resp.StatusCode)
 	}
 
-	// Read the response body into a byte array
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		// Log the error if reading the response body failed
-		log.Printf("Error reading response body: %v", err)
-		return nil, err
+	data, errtwo := ioutil.ReadAll(resp.Body)
+	
+	if errtwo == nil {
+	log.Printf("error is empty: %v", err)
 	}
-
+	
 	return data, nil
 }
 
-// ServeHTTP handles HTTP requests and writes an XML response with nearby places information
+// GET REQUEST
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Set the response header to specify the content type
-	w.Header().Set("Content-Type", "application/xml")
+	w.Header().Set("Content-Type", "application/xml") // ADJUST YOUR RETURN TYPE HERE can be e.g application/json
 	w.WriteHeader(http.StatusOK)
 
-	// Get the query parameters from the request URL
 	key := r.URL.Query().Get("key")
 	value := r.URL.Query().Get("value")
 	radius := r.URL.Query().Get("radius")
 
-	// Parse the radius parameter into an integer
+	// Radius is parsed here
 	radiusInt, err := strconv.ParseInt(radius, 10, 64)
 	if err != nil {
-		// Handle any errors that occur during parsing
-		log.Printf("Error parsing radius parameter: %v", err)
+		// handle error
+		log.Printf("error in serve http is: %v", err)
+		
 	}
 
-	// Get nearby places information based on the query parameters
 	places := getNearbyPlaces(key, value, radiusInt)
 
-	// Construct an XML response with the query parameters and nearby places information
 	xmlPrint :=
+
 		"<company>Shahsuvarov corp.</company>" + "\n" +
-			"\t<product>Geoloc</product>" + "\n" +
+			"\t<product>GeoLoc.</product>" + "\n" +
 			"\t<key>" + key + "</key>\n" +
 			"\t<value>" + value + "</value>\n" +
 			"\t<radius>" + radius + "</radius>\n" +
 			"\t<places>" + places + "</places>"
 
-	// Write the XML response to the response writer
-	data, err := w.Write([]byte(xmlPrint))
+	data,err := w.Write([]byte(xmlPrint))
+	
 	if data != 0 {
-		// Log the number of bytes written to the response writer
-		log.Printf("Number of bytes written to response writer: %v", data)
+	log.Printf("Data is : %v", data)
 	}
+	
 	if err == nil {
-		// Log any errors that occur during writing to the response writer
-		log.Printf("Error writing XML response to response writer: %v", err)
+	log.Printf("No errors ocurred: %v", err)
 	}
+
 }
 
-// getLatLonJSON sends a POST request to the Google Geocoding API to obtain the
-// latitude and longitude as JSON.
+// For getting the latitude and longitude as JSON from GOOGLE GEOCODING API request
 func getLatLonJSON() string {
-	// Define the URL for the Google Geocoding API.
-	url := "https://www.googleapis.com/geolocation/v1/geolocate?key=API_KEY"
+	// POST REQUEST TO GOOGLE GEOCODING API WITH API KEY
+	url := "https://www.googleapis.com/geolocation/v1/geolocate?key=GOOGLE_MAP_API_KEY"
 
-	// Define an empty JSON string and create a new HTTP request with it.
 	var jsonStr = []byte(``)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-
-	// Set the necessary headers for the request.
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 	req.Header.Set("Vary", "Origin")
 	req.Header.Set("Vary", "X-Origin")
@@ -114,27 +95,19 @@ func getLatLonJSON() string {
 	req.Header.Set("X-Content-Type-Options", "nosniff")
 	req.Header.Set("Alt-Svc", "h3-27="+":443"+"; ma=2592000,h3-25="+":443"+"; ma=2592000,h3-T050="+":443"+"; ma=2592000,h3-Q050="+":443"+"; ma=2592000,h3-Q046="+":443"+"; ma=2592000,h3-Q043="+":443"+"; ma=2592000,quic="+":443"+"; ma=2592000; v="+"46,43"+"")
 	req.Header.Set("Transfer-Encoding", "chunked")
-
-	// Check if there was an error creating the request and log it if so.
+	
 	if err != nil {
-		log.Printf("error creating HTTP request: %v", err)
+	log.Printf("error in getting the lat and lng: %v", err)
 	}
 
-	// Send the HTTP request and check for errors.
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Printf("error sending HTTP request: %v", err)
-		return ""
+		panic(err)
 	}
 	defer resp.Body.Close()
 
-	// Read the response body and convert it to a string.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("error reading response body: %v", err)
-		return ""
-	}
+	body, _ := ioutil.ReadAll(resp.Body)
 
 	return string(body)
 }
@@ -148,57 +121,37 @@ type UserGeolocation struct {
 	} `json:"location"`
 }
 
-// getLatLon parses the latitude and longitude from structured JSON.
+// For parsing the latitude and longitude from strutured JSON
 func getLatLon(locJSON string) (float64, float64) {
-	// Initialize a UserGeolocation object.
+
 	var loc UserGeolocation
-
-	// Unmarshal the JSON into the UserGeolocation object.
 	err := json.Unmarshal([]byte(locJSON), &loc)
+	
 	if err != nil {
-		// Handle any errors that occurred during the unmarshaling process.
-		log.Printf("Error in getting lat and lng: %v", err)
+	log.Printf("err in getting lat and lng: %v", err)
 	}
-
-	// Return the latitude and longitude.
+	
 	return loc.Location.Lat, loc.Location.Lng
 }
 
-// getNearbyPlaces is a function that takes in user's key and value input as well as a radius, gets the user's latitude and longitude using
-// the Google Geocoding API, and uses these values to make a request to the Google Places API to get nearby places. It then returns the
-// response data as a string.
-func getNearbyPlaces(key string, value string, radius int64) string {
-	// Check if the data is already cached
-	cacheKey := fmt.Sprintf("%s_%s_%d", key, value, radius)
-	cacheMutex.Lock()
-	if data, ok := cache[cacheKey]; ok {
-		cacheMutex.Unlock()
-		return data
-	}
-	cacheMutex.Unlock()
+func getNearbyPlaces(keyInput, valueInput string, radius int64) string {
 
-	// If the data is not cached, fetch it from the Overpass API
-	LAT, LNG := getLatLon()
-	data, err := getContent(strconv.FormatInt(radius, 10), LAT, LNG, key, value)
+	var LAT, LNG = getLatLon(getLatLonJSON())
+	fmt.Printf("LAT: %v\nLNG: %v", LAT, LNG)
+	data, err := getContent(fmt.Sprintf("%v",radius), LAT, LNG, keyInput, valueInput);
 	if err != nil {
-		log.Printf("Error getting content: %v", err)
+		log.Printf("Failed to get XML: %v", err)
 		return ""
-	}
+	} 
+		return string(data)
+	
 
-	// Convert the response data to a string and cache it
-	places := string(data)
-	cacheMutex.Lock()
-	cache[cacheKey] = places
-	cacheMutex.Unlock()
-
-	return places
 }
 
-// main function runs the HTTP server to listen on incoming requests on port 8081
 func main() {
 
 	s := &server{} // TO RUN THE SERVER TO HAVE HTTP REQUESTS FROM OUTSIDE (E.G POSTMAN)
-	http.Handle("/geoloc/", s)
+	http.Handle("/location-api/", s)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
